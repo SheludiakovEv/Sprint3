a9bcdecb0781c188354ae55ebe678707789b5872
Evgeniy Sheludiakov <sheludiakovev@mail.ru>
2022-10-24 01:43:53 +0500

    add allure report

---
+++ src/test/java/courierTest/CourierBaseTest.java
@@ -0,0 +1,15 @@
+package courierTest;
+
+import client.Courier;
+import org.junit.After;
+import org.junit.Before;
+
+public class CourierBaseTest {
+    private Courier courier;
+
+    @Before
+    public void setUpCourier() {
+        courier = new Courier();
+    }
+
+}
